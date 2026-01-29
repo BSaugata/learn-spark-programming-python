2e5cb6255643fc4d474307f999b9acfbaec582df
Saugata Bhattacharya <saugata_bhattacharya@external.mckinsey.com>
2026-01-29 08:46:30 +0000

    delete old dataset

--- ratings-counter.py
+++ ratings-counter.py
@@ -1,13 +1,17 @@
 from pyspark import SparkConf, SparkContext
-import collections
+import collections, time
 
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
+start_time = time.time()
 
 lines = sc.textFile("data/input/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
+
 sortedResults = collections.OrderedDict(sorted(result.items()))
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
+
+print("--- %s seconds ---" % (time.time() - start_time))
